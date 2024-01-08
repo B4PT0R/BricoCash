ae763386c9f7c6e861a8f52d7d5af3484f68ccaf
B4PT0R <bferrand.maths@gmail.com>
2024-01-08 15:40:01 +0100

    //

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import os
+from objdict_bf import objdict
 
 _root_=os.path.dirname(os.path.abspath(__file__))
 def root_join(*args):
@@ -7,13 +8,10 @@ def root_join(*args):
 
 def vspace():
     return st.markdown("""<p><br></p>""",unsafe_allow_html=True)
-    
-
-vspace()
 
 _,c,_=st.columns([27,46,27])
 with c:
-    st.image(root_join("app_images/bricocash_logo.png"),width=300)
+    st.image(root_join("app_images/bricocash_logo.png"),width=200)
 
 st.write("---")
 
@@ -24,11 +22,7 @@ st.write("---")
 st.write("Vous cherchez un rayon ?")
 product=st.selectbox(label="Type de produit",options=["","peinture","quincaillerie","électro-portatif"])
 
-location={
-    "peinture":"dans l'allée 1",
-    "quincaillerie":"dans la section bois",
-    "électro-portatif":"à l'entrée près des caisses"
-}
+location=objdict.load("locations.json")
 
 if product and (product in location):
     st.write(f"Vous trouverez ce produit {location[product]}.")
